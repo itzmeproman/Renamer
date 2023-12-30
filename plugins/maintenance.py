1bd02fa5d87b4c25bbf9ab5a2ce459e066b3d952
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-12-30 22:40:47 +0530

    Create maintenance.py

---
+++ plugins/maintenance.py
@@ -0,0 +1,29 @@
+# main_bot.py
+from config import ADMINS
+from pyrogram import Client as app, filters
+from helper.maindb import MaintenanceManager
+
+# Use the maintenance_manager instance
+maintenance_manager = MaintenanceManager()
+
+# Wrapper for commands affected by maintenance mode
+maintenance_check_wrapper = maintenance_manager.maintenance_mode_check
+
+
+@app.on_message(filters.private & filters.command("maintenance"))
+async def maintenance_command(client, message):
+    user_id = message.from_user.id
+
+    if user_id == ADMINS:  # Replace YOUR_ADMIN_USER_ID with the actual admin user ID
+        action = message.text.split("/maintenance", 1)[1].strip().lower()
+
+        if action == "on":
+            maintenance_manager.turn_on_maintenance()
+            await message.reply_text("Maintenance mode is ON")
+        elif action == "off":
+            maintenance_manager.turn_off_maintenance()
+            await message.reply_text("Maintenance mode is OFF")
+        else:
+            await message.reply_text("Invalid action. Please use '/maintenance on' or '/maintenance off'.")
+    else:
+        await message.reply_text("You are not authorized to use this command.")
