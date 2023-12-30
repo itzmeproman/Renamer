d29547ed57a6765a88718b1bfc10c3c2defbfe04
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-12-30 22:29:01 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
-from pyrogram.types import Message
+from pyrogram.types import InputMediaDocument
 from PIL import Image
 from datetime import datetime
 
@@ -13,25 +13,49 @@ from helper.database import db
 import os
 import time
 import re
-from collections import deque
+from queue import Queue
 
-renaming_operations = {}
-file_queue = deque()
+# Create a queue for handling renaming operations
+renaming_queue = Queue()
 
+# Pattern 1: S01E02 or S01EP02
+pattern1 = re.compile(r'S(\d+)(?:E|EP)(\d+)')
 
-# Quality Patterns
+# Pattern 2: S01 E02 or S01 EP02 or S01 - E01 or S01 - EP02
+pattern2 = re.compile(r'S(\d+)\s*(?:E|EP|-\s*EP)(\d+)')
+
+# Pattern 3: Episode Number After "E" or "EP"
+pattern3 = re.compile(r'(?:[([<{]?\s*(?:E|EP)\s*(\d+)\s*[)\]>}]?)')
+
+# Pattern 3_2: episode number after - [hyphen]
+pattern3_2 = re.compile(r'(?:\s*-\s*(\d+)\s*)')
+
+# Pattern 4: S2 09 ex.
+pattern4 = re.compile(r'S(\d+)[^\d]*(\d+)', re.IGNORECASE)
+
+# Pattern X: Standalone Episode Number
+patternX = re.compile(r'(\d+)')
+
+# QUALITY PATTERNS 
+# Pattern 5: 3-4 digits before 'p' as quality
 pattern5 = re.compile(r'\b(?:.*?(\d{3,4}[^\dp]*p).*?|.*?(\d{3,4}p))\b', re.IGNORECASE)
+# Pattern 6: Find 4k in brackets or parentheses
 pattern6 = re.compile(r'[([<{]?\s*4k\s*[)\]>}]?', re.IGNORECASE)
+# Pattern 7: Find 2k in brackets or parentheses
 pattern7 = re.compile(r'[([<{]?\s*2k\s*[)\]>}]?', re.IGNORECASE)
+# Pattern 8: Find HdRip without spaces
 pattern8 = re.compile(r'[([<{]?\s*HdRip\s*[)\]>}]?|\bHdRip\b', re.IGNORECASE)
+# Pattern 9: Find 4kX264 in brackets or parentheses
 pattern9 = re.compile(r'[([<{]?\s*4kX264\s*[)\]>}]?', re.IGNORECASE)
+# Pattern 10: Find 4kx265 in brackets or parentheses
 pattern10 = re.compile(r'[([<{]?\s*4kx265\s*[)\]>}]?', re.IGNORECASE)
 
-
 def extract_quality(filename):
+    # Try Quality Patterns
     match5 = re.search(pattern5, filename)
     if match5:
-        return match5.group(1) or match5.group(2)
+        quality5 = match5.group(1) or match5.group(2)  # Extracted quality from both patterns
+        return quality5
 
     match6 = re.search(pattern6, filename)
     if match6:
@@ -51,130 +75,126 @@ def extract_quality(filename):
 
     match10 = re.search(pattern10, filename)
     if match10:
-        return "4kx265"
+        return "4kx265"    
 
+    # Return "Unknown" if no pattern matches
     return "Unknown"
 
-
-def extract_episode_number(filename):
-    pattern1 = re.compile(r'S(\d+)(?:E|EP)(\d+)')
-    pattern2 = re.compile(r'S(\d+)\s*(?:E|EP|-\s*EP)(\d+)')
-    pattern3 = re.compile(r'(?:[([<{]?\s*(?:E|EP)\s*(\d+)\s*[)\]>}]?)')
-    pattern3_2 = re.compile(r'(?:\s*-\s*(\d+)\s*)')
-    pattern4 = re.compile(r'S(\d+)[^\d]*(\d+)', re.IGNORECASE)
-    patternX = re.compile(r'(\d+)')
-
+def extract_episode_number(filename):    
+    # Try Pattern 1
     match = re.search(pattern1, filename)
     if match:
-        return match.group(2)
-
+        return match.group(2)  # Extracted episode number
+    
+    # Try Pattern 2
     match = re.search(pattern2, filename)
     if match:
-        return match.group(2)
+        return match.group(2)  # Extracted episode number
 
+    # Try Pattern 3
     match = re.search(pattern3, filename)
     if match:
-        return match.group(1)
+        return match.group(1)  # Extracted episode number
 
+    # Try Pattern 3_2
     match = re.search(pattern3_2, filename)
     if match:
-        return match.group(1)
-
+        return match.group(1)  # Extracted episode number
+        
+    # Try Pattern 4
     match = re.search(pattern4, filename)
     if match:
-        return match.group(2)
+        return match.group(2)  # Extracted episode number
 
+    # Try Pattern X
     match = re.search(patternX, filename)
     if match:
-        return match.group(1)
-
+        return match.group(1)  # Extracted episode number
+        
+    # Return None if no pattern matches
     return None
 
+@Client.on_message(filters.private & filters.command("autorename"))
+async def auto_rename_command(client, message):
+    user_id = message.from_user.id
+    format_template = message.text.split("/autorename", 1)[1].strip()
 
-async def on_task_complete(client, message_id):
-    ok = data_check(message_id)
-    file_queue.popleft()
-    await process_queue(client, message_id)
+    await db.set_format_template(user_id, format_template)
 
+    await message.reply_text("Auto rename format updated successfully!")
 
-def data_check(message_id):
-    for i in file_queue:
-        if message_id == i.message_id:
-            return i
+@Client.on_message(filters.private & filters.command("setmedia"))
+async def set_media_command(client, message):
+    user_id = message.from_user.id
+    media_type = message.text.split("/setmedia", 1)[1].strip().lower()
 
+    await db.set_media_preference(user_id, media_type)
 
-async def add_task(message, user_id):
-    file_queue.append(message)
-    await process_queue(message.chat.id)
+    await message.reply_text(f"Media preference set to: {media_type}")
 
-
-async def process_queue(client, chat_id):
-    if file_queue:
-        message = file_queue.popleft()
-        await process_file(client, message, chat_id)
-
-
-async def process_file(client, message, chat_id):
+# Inside the handler for file uploads
+@Client.on_message(filters.private & (filters.document | filters.video | filters.audio))
+async def auto_rename_files(client, message):
     user_id = message.from_user.id
     format_template = await db.get_format_template(user_id)
     media_preference = await db.get_media_preference(user_id)
 
+    if not format_template:
+        return await message.reply_text("Please set an auto rename format first using /autorename")
+
     if message.document:
         file_id = message.document.file_id
         file_name = message.document.file_name
-        media_type = media_preference or "document"
+        media_type = media_preference or "document"  # Use preferred media type or default to document
     elif message.video:
         file_id = message.video.file_id
         file_name = f"{message.video.file_name}.mp4"
-        media_type = media_preference or "video"
+        media_type = media_preference or "video"  # Use preferred media type or default to video
     elif message.audio:
         file_id = message.audio.file_id
         file_name = f"{message.audio.file_name}.mp3"
-        media_type = media_preference or "audio"
+        media_type = media_preference or "audio"  # Use preferred media type or default to audio
     else:
         return await message.reply_text("Unsupported file type")
 
     print(f"Original File Name: {file_name}")
 
-    if file_id in renaming_operations:
-        elapsed_time = (datetime.now() - renaming_operations[file_id]).seconds
-        if elapsed_time < 10:
-            print("File is being ignored as it is currently being renamed or was renamed recently.")
-            return
+    if file_id in renaming_queue.queue:
+        return  # Exit the handler if the file is already in the queue
 
-    renaming_operations[file_id] = datetime.now()
+    renaming_queue.put(file_id)
 
     episode_number = extract_episode_number(file_name)
+    
     print(f"Extracted Episode Number: {episode_number}")
-
+    
     if episode_number:
         placeholders = ["episode", "Episode", "EPISODE", "{episode}"]
         for placeholder in placeholders:
             format_template = format_template.replace(placeholder, str(episode_number), 1)
-
+            
         quality_placeholders = ["quality", "Quality", "QUALITY", "{quality}"]
         for quality_placeholder in quality_placeholders:
             if quality_placeholder in format_template:
                 extracted_qualities = extract_quality(file_name)
                 if extracted_qualities == "Unknown":
-                    await message.reply_text("Unable to extract quality. Renaming as 'Unknown'...")
-                    del renaming_operations[file_id]
+                    await message.reply_text("I wasn't able to extract the quality properly. Renaming as 'Unknown'...")
+                    renaming_queue.get()  # Remove from the queue
                     return
-
-                format_template = format_template.replace(quality_placeholder, "".join(extracted_qualities))
-
+                
+                format_template = format_template.replace(quality_placeholder, "".join(extracted_qualities))           
+            
         _, file_extension = os.path.splitext(file_name)
         new_file_name = f"{format_template}{file_extension}"
         file_path = f"downloads/{new_file_name}"
+        file = message
 
         download_msg = await message.reply_text(text="Trying to download...")
         try:
-            path = await client.download_media(message=message, file_name=file_path,
-                                               progress=progress_for_pyrogram,
-                                               progress_args=("Download Started....", download_msg, time.time()))
+            path = await client.download_media(message=file, file_name=file_path, progress=progress_for_pyrogram, progress_args=("Download Started....", download_msg, time.time()))
         except Exception as e:
-            del renaming_operations[file_id]
-            return await download_msg.edit(e)
+            renaming_queue.get()  # Remove from the queue
+            return await download_msg.edit(e)     
 
         duration = 0
         try:
@@ -190,8 +210,7 @@ async def process_file(client, message, chat_id):
         c_caption = await db.get_caption(message.chat.id)
         c_thumb = await db.get_thumbnail(message.chat.id)
 
-        caption = c_caption.format(filename=new_file_name, filesize=humanbytes(message.document.file_size),
-                                    duration=convert(duration)) if c_caption else f"**{new_file_name}**"
+        caption = c_caption.format(filename=new_file_name, filesize=humanbytes(message.document.file_size), duration=convert(duration)) if c_caption else f"**{new_file_name}**"
 
         if c_thumb:
             ph_path = await client.download_media(c_thumb)
@@ -209,7 +228,7 @@ async def process_file(client, message, chat_id):
             type = media_type
             if type == "document":
                 await client.send_document(
-                    chat_id,
+                    message.chat.id,
                     document=file_path,
                     thumb=ph_path,
                     caption=caption,
@@ -218,7 +237,7 @@ async def process_file(client, message, chat_id):
                 )
             elif type == "video":
                 await client.send_video(
-                    chat_id,
+                    message.chat.id,
                     video=file_path,
                     caption=caption,
                     thumb=ph_path,
@@ -228,7 +247,7 @@ async def process_file(client, message, chat_id):
                 )
             elif type == "audio":
                 await client.send_audio(
-                    chat_id,
+                    message.chat.id,
                     audio=file_path,
                     caption=caption,
                     thumb=ph_path,
@@ -240,58 +259,14 @@ async def process_file(client, message, chat_id):
             os.remove(file_path)
             if ph_path:
                 os.remove(ph_path)
-            del renaming_operations[file_id]
+            renaming_queue.get()  # Remove from the queue
             return await upload_msg.edit(f"Error: {e}")
 
-        await download_msg.delete()
+        await upload_msg.delete() 
         os.remove(file_path)
         if ph_path:
             os.remove(ph_path)
 
-        del renaming_operations[file_id]
-
-
-@Client.on_message(filters.private & filters.command("autorename"))
-async def auto_rename_command(client, message):
-    user_id = message.from_user.id
-    format_template = message.text.split("/autorename", 1)[1].strip()
-    await db.set_format_template(user_id, format_template)
-    await message.reply_text("Auto rename format updated successfully!")
-
-
-@Client.on_message(filters.private & filters.command("setmedia"))
-async def set_media_command(client, message):
-    user_id = message.from_user.id
-    media_type = message.text.split("/setmedia", 1)[1].strip().lower()
-    await db.set_media_preference(user_id, media_type)
-    await message.reply_text(f"Media preference set to: {media_type}")
-
-
-@Client.on_message(filters.private & (filters.document | filters.video | filters.audio))
-async def handle_file(client, message):
-    user_id = message.from_user.id
-    await add_task(message, user_id)
-
-
-@Client.on_message(filters.private & filters.command("queue"))
-async def queue_command(client, message):
-    user_id = message.from_user.id
-    queue_size = len(file_queue)
-    await message.reply_text(f"Current queue size: {queue_size}")
-
-
-@Client.on_message(filters.private & filters.command("processqueue"))
-async def process_queue_command(client, message):
-    user_id = message.from_user.id
-    await process_queue(client, user_id)
-
-
-@Client.on_message(filters.private & filters.command("cancelqueue"))
-async def cancel_queue_command(client, message):
-    global file_queue
-    user_id = message.from_user.id
-    file_queue = deque()
-    await message.reply_text("Queue cleared successfully!")
-
-
+        # Remove from the queue after successful renaming
+        renaming_queue.get()
 
