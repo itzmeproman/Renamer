3b750b0e66506eee9ce6e76c09ca89ddd94686c7
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-12-31 08:19:32 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -1,13 +1,11 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery, Message, InputMediaPhoto, InputMediaVideo
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, InputMediaVideo, CallbackQuery
+
 from helper.database import db
 from config import Config, Txt
 from helper.maindb import MaintenanceManager
 
-# Use the maintenance_manager instance
 maintenance_manager = MaintenanceManager()
-
-# Wrapper for commands affected by maintenance mode
 maintenance_check_wrapper = maintenance_manager.maintenance_mode_check
 
 @Client.on_message(filters.private & filters.command("start"))
@@ -15,18 +13,14 @@ maintenance_check_wrapper = maintenance_manager.maintenance_mode_check
 async def start(client, message):
     user = message.from_user
     await db.add_user(client, message)
+    
     button = InlineKeyboardMarkup([
-        [
-            InlineKeyboardButton("Cᴏᴍᴍᴀɴᴅs", callback_data='commands')
-        ],
-        [
-            InlineKeyboardButton('Uᴘᴅᴀᴛᴇs', url='https://t.me/UchihaPoliceUpdates'),
-            InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ', url='https://t.me/UchihaPolice_Support')
-        ],
-        [
-            InlineKeyboardButton('Hᴇʟᴘ', callback_data='about')
-        ]
+        [InlineKeyboardButton("Commands", callback_data='commands')],
+        [InlineKeyboardButton('Updates', url='https://t.me/UchihaPoliceUpdates'),
+         InlineKeyboardButton('Support', url='https://t.me/UchihaPolice_Support')],
+        [InlineKeyboardButton('Help', callback_data='about')]
     ])
+    
     if Config.START_PIC:
         await message.reply_video(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)
     else:
@@ -42,37 +36,23 @@ async def cb_handler(client, query: CallbackQuery):
             text=Txt.START_TXT.format(query.from_user.mention),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
-                [
-                    InlineKeyboardButton("Cᴏᴍᴍᴀɴᴅs", callback_data='commands')
-                ],
-                [
-                    InlineKeyboardButton('Uᴩᴅᴀᴛᴇꜱ', url='https://t.me/UchihaPoliceUpdates'),
-                    InlineKeyboardButton('Sᴜᴩᴩᴏʀᴛ', url='https://t.me/UchihaPolice_Support')
-                ],
-                [
-                    InlineKeyboardButton('Hᴇʟᴘ', callback_data='about')
-                ]
+                [InlineKeyboardButton("Commands", callback_data='commands')],
+                [InlineKeyboardButton('Updates', url='https://t.me/UchihaPoliceUpdates'),
+                 InlineKeyboardButton('Support', url='https://t.me/UchihaPolice_Support')],
+                [InlineKeyboardButton('Help', callback_data='about')]
             ])
         )
     elif data == "about":
         media = InputMediaVideo(Config.START_PIC)
 
-        await query.message.edit_media(
-            media=media,
-        )
+        await query.message.edit_media(media=media)
         await query.message.edit_caption(
             caption=Txt.ABOUT_TXT.format(client.mention),
             reply_markup=InlineKeyboardMarkup([
-                [
-                    InlineKeyboardButton("Sᴇᴛᴜᴩ Aᴜᴛᴏʀᴇɴᴀᴍᴇ Fᴏʀᴍᴀᴛ", callback_data='file_names')
-                ],
-                [
-                    InlineKeyboardButton('Tʜᴜᴍʙɴᴀɪʟ', callback_data='thumbnail'),
-                    InlineKeyboardButton('Sᴇǫᴜᴇɴᴄᴇ', callback_data='sequence')
-                ],
-                [
-                    InlineKeyboardButton('Hᴏᴍᴇ', callback_data='start')
-                ]
+                [InlineKeyboardButton("Set Username Format", callback_data='file_names')],
+                [InlineKeyboardButton('Thumbnail', callback_data='thumbnail'),
+                 InlineKeyboardButton('Sequence', callback_data='sequence')],
+                [InlineKeyboardButton('Home', callback_data='start')]
             ])
         )
     elif data == "sequence":
@@ -80,25 +60,18 @@ async def cb_handler(client, query: CallbackQuery):
             text=Txt.SEQUENCE_TXT,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
-                [
-                    InlineKeyboardButton("Rɪɴ Nᴏʜᴀʀᴀ", url='https://t.me/RinNohara_xBot')
-                ],
-                [
-                    InlineKeyboardButton("Cʟᴏꜱᴇ", callback_data="close"),
-                    InlineKeyboardButton("Bᴀᴄᴋ", callback_data="start")
-                ]
+                [InlineKeyboardButton("Rɪɴ Nᴏʜᴀʀᴀ", url='https://t.me/RinNohara_xBot')],
+                [InlineKeyboardButton("Close", callback_data="close"),
+                 InlineKeyboardButton("Back", callback_data="start")]
             ])
         )
-
     elif data == "commands":
         await query.message.edit_text(
             text=Txt.COMMANDS_TXT,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
-                [
-                    InlineKeyboardButton("Cʟᴏꜱᴇ", callback_data="close"),
-                    InlineKeyboardButton("Bᴀᴄᴋ", callback_data="start")
-                ]
+                [InlineKeyboardButton("Close", callback_data="close"),
+                 InlineKeyboardButton("Back", callback_data="start")]
             ])
         )
     elif data == "file_names":
@@ -107,35 +80,26 @@ async def cb_handler(client, query: CallbackQuery):
             text=Txt.FILE_NAME_TXT.format(format_template=format_template),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
-                [
-                    InlineKeyboardButton("Cʟᴏꜱᴇ", callback_data="close"),
-                    InlineKeyboardButton("Bᴀᴄᴋ", callback_data="about")
-                ]
+                [InlineKeyboardButton("Close", callback_data="close"),
+                 InlineKeyboardButton("Back", callback_data="about")]
             ])
         )
     elif data == "thumbnail":
         user_thumbnail = await db.get_thumbnail(user_id)
 
         if user_thumbnail:
-            await query.message.edit_media(
-                media=InputMediaPhoto(user_thumbnail),
-            )
+            await query.message.edit_media(media=InputMediaPhoto(user_thumbnail))
         else:
             await query.message.edit_text(text=Txt.THUMB_TXT, reply_markup=InlineKeyboardMarkup([
-                [
-                    InlineKeyboardButton("Cʟᴏꜱᴇ", callback_data="close"),
-                    InlineKeyboardButton("Bᴀᴄᴋ", callback_data="about"),
-                ]
+                [InlineKeyboardButton("Close", callback_data="close"),
+                 InlineKeyboardButton("Back", callback_data="about")]
             ]))
         await query.message.edit_caption(
             caption=Txt.THUMB_TXT,
             reply_markup=InlineKeyboardMarkup([
-                [
-                    InlineKeyboardButton("Cʟᴏꜱᴇ", callback_data="close"),
-                    InlineKeyboardButton("Bᴀᴄᴋ", callback_data="about"),
-                ]
-            ]
-                                             )
+                [InlineKeyboardButton("Close", callback_data="close"),
+                 InlineKeyboardButton("Back", callback_data="about")]
+            ])
         )
     elif data == "close":
         try:
