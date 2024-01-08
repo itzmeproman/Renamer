1fd75c2ea74c96c1e3586c8b7c6a92368e77d2ce
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2024-01-09 00:01:57 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -14,7 +14,7 @@ import asyncio
 
 renaming_operations = {}
 file_count_limit = 100
-sleep_duration = 30 * 60 
+sleep_duration = 20 * 60 
 
 user_file_counts = {}
 
@@ -163,10 +163,6 @@ async def auto_rename_files(client, message):
 
                 format_template = format_template.replace(quality_placeholder, "".join(extracted_qualities))
 
-        title = re.sub(r'[^a-zA-Z0-9\s]', '', os.path.splitext(file_name)[0])  # Remove special characters from title
-
-        format_template = format_template.replace("{title}", title)
-
         _, file_extension = os.path.splitext(file_name)
         new_file_name = f"{format_template}{file_extension}"
         file_path = f"downloads/{new_file_name}"
@@ -251,4 +247,3 @@ async def auto_rename_files(client, message):
             os.remove(ph_path)
 
         del renaming_operations[file_id]
-    
