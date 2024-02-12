cd9c8835950eb102293e7ed12ed7f27ced60daeb
SiamRin33 <149977276+SiamRin33@users.noreply.github.com>
2024-02-13 03:15:47 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,20 +4,20 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "22418774")
-    API_HASH  = os.environ.get("API_HASH", "d8c8dab274f9a811814a6a96d044028e")
+    API_ID    = os.environ.get("API_ID", "")
+    API_HASH  = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
     # database config
-    DB_NAME = os.environ.get("DB_NAME","obi")     
-    DB_URL  = os.environ.get("DB_URL","mongodb+srv://mehtadmphta33:Mehtab1234@cluster0.bfsb3oq.mongodb.net/?retryWrites=true&w=majority")
+    DB_NAME = os.environ.get("DB_NAME","miku")     
+    DB_URL  = os.environ.get("DB_URL","")
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/65fc609367ee91c4ba20b.mp4")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '760067286').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "UchihaPoliceUpdates") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002008965715"))
+    START_PIC   = os.environ.get("START_PIC", "https://te.legra.ph/file/0fd5da0ff319926b6e0e7.jpg")
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5971676967').split()]
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "Anime_Kun_Channel") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
@@ -31,20 +31,20 @@ class Txt(object):
     """
         
     START_TXT = """Hᴇʏ Tʜᴇʀᴇ {}!!
-Oʀᴇ Nᴏ Nᴀᴡᴀ **Eʀᴇɴ Yᴇᴀɢᴇʀ**, ᴀ ꜰᴀsᴛ ᴀᴜᴛᴏʀᴇɴᴀᴍᴇ ʙᴏᴛ ᴡʜᴏ's ɢᴏɴɴᴀ ᴍᴀᴋᴇ ʏᴏᴜʀ ꜰɪʟᴇs/ᴠɪᴅᴇᴏ ʀᴇɴᴀᴍɪɴɢ ᴘʀᴏᴄᴇss ʀᴇᴀʟ qᴜɪᴄᴋ ᴀɴᴅ ᴇᴀsʏ.
+Oʀᴇ Nᴏ Nᴀᴡᴀ **Mɪᴋᴜ Nᴀᴋᴀɴᴏ**, ᴀ ꜰᴀsᴛ ᴀᴜᴛᴏʀᴇɴᴀᴍᴇ ʙᴏᴛ ᴡʜᴏ's ɢᴏɴɴᴀ ᴍᴀᴋᴇ ʏᴏᴜʀ ꜰɪʟᴇs/ᴠɪᴅᴇᴏ ʀᴇɴᴀᴍɪɴɢ ᴘʀᴏᴄᴇss ʀᴇᴀʟ qᴜɪᴄᴋ ᴀɴᴅ ᴇᴀsʏ.
 
 ᴛᴏ ᴋɴᴏᴡ ᴡʜᴀᴛ I'ᴍ ᴄᴀᴘᴀʙʟᴇ ᴏꜰ ᴏᴛʜᴇʀ ᴛʜᴀɴ ᴍᴀss-ᴍᴀssᴀᴄʀᴇ, ʙᴇᴛᴛᴇʀ ᴜsᴇ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ʙᴇʟᴏᴡ.. 🕊️"""
     
     FILE_NAME_TXT = """
-    <u><b>SETUP AUTO RENAME FORMAT</b></u>\n\nUse These Keywords To Setup Custom File Name\n\n➝ episode :- to replace episode number\n➝ quality :- to replace video resolution\n\n‣ <b>Example :</b> /autorename [CW] Attack on Titan S1 - episode [quality] [Dual] @Anime_Channel.mkv\n\n‣ <b>Your Current Rename Format :</b> {format_template}
+    <u><b>SETUP AUTO RENAME FORMAT</b></u>\n\nUse These Keywords To Setup Custom File Name\n\n➝ episode :- to replace episode number\n➝ quality :- to replace video resolution\n\n‣ <b>Example :</b> /autorename [CW] Attack on Titan S1 - episode [quality] [Dual] @Anime_Kun_Channel.mkv\n\n‣ <b>Your Current Rename Format :</b> {format_template}
     """
     
     ABOUT_TXT = f"""<b>╭───────────⍟
-├🦥 Owner: <a href=http://t.me/Xenov7x>𝙓𝙚𝙣𝙤𝙫</a>
+├🦥 Owner: <a href=http://t.me/Chowdhury_Siam>Sɪᴀᴍ Cʜᴏᴡᴅʜᴜʀʏ</a>
 ├📕 Library: <a href=https://github.com/pyrogram>Pyʀᴏɢʀᴀᴍ</a>
 ├✏️ Language: <a href=https://www.python.org>Pyᴛʜᴏɴ3</a>
 ├💾 Data Base: <a href=https://cloud.mongodb.com>Mᴏɴɢᴏ DB</a>
-├📊 Build Version: `EREH V1.7.0`
+├📊 Build Version: `Mɪᴋᴜ V1.7.0`
 ╰───────────────⍟ 
 """
 
@@ -55,7 +55,6 @@ Oʀᴇ Nᴏ Nᴀᴡᴀ **Eʀᴇɴ Yᴇᴀɢᴇʀ**, ᴀ ꜰᴀsᴛ ᴀᴜᴛᴏ�
 
     PREMIUM_TXT = """Not Decided Yet"""
 
-#⚠️ Dᴏɴ'ᴛ Rᴇᴍᴏᴠᴇ Oᴜʀ Cʀᴇᴅɪᴛꜱ @ᴩyʀᴏ_ʙᴏᴛᴢ🙏🥲
     COMMANDS_TXT = """🌌 How To Set Thumbnail
   
 • /start the bot and send any photo to automatically set the thumbnail.
@@ -71,7 +70,7 @@ Example: /set_caption 📕 File Name: {filename}
 ⏰ Duration: {duration}
 
 ✏️ How To Rename A File
-• First set /autorename format then send your files. Eg: /autorename [S01-episode] Attack on Titan [quality] [Dual] @Anime_Channel.mkv
+• First set /autorename format then send your files. Eg: /autorename [S01-episode] Attack on Titan [quality] [Dual] @Anime_Kun_Channel.mkv
 
 ‣ Available Variables
 » <code>episode</code>
